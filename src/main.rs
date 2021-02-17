035a2875d323e6f5da656cba3dd5f44e7773ad0f
Ralph de Horde <ralphdehorde@gmail.com>
2021-02-17 20:50:02 +0100

    Finished singlethreaded webserver
    Added 404 page and optimized the if statement.

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,19 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
     println!("{}", String::from_utf8_lossy(&buffer));
 
-    let contents = fs::read_to_string("response_page/index.html").unwrap();
+    let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "response_page/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "response_page/404.html")
+    };
+    let contents = fs::read_to_string(file_name).unwrap();
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
