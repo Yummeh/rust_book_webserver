3539458cfa5966c05eca725e8b8d295ef5ac05db
Ralph de Horde <ralphdehorde@gmail.com>
2021-02-18 23:48:13 +0100

    Finished multithreaded webserver

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,18 +1,27 @@
+use core::panic;
 use std::{
     fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
-use fs::read_to_string;
+use rust_book_webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = match ThreadPool::new(4) {
+        Ok(n) => n,
+        Err(e) => panic!("{}", e),
+    };
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        })
     }
 }
 
@@ -21,13 +30,17 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    println!("{}", String::from_utf8_lossy(&buffer));
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "response_page/index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "response_page/index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "response_page/404.html")
     };
+
     let contents = fs::read_to_string(file_name).unwrap();
 
     let response = format!("{}{}", status_line, contents);
---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+use std::{
+    fmt,
+    sync::mpsc,
+    thread::{self, JoinHandle},
+    write,
+};
+
+use mpsc::Receiver;
+use std::sync::{Arc, Mutex};
+
+/// Pool of threads
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    /// The 'new' function will panic if the size is zero.
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if !(size > 0) {
+            return Err(PoolCreationError);
+        }
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+        for i in 0..size {
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
+        }
+
+        Ok(ThreadPool {
+            workers: workers,
+            sender: sender,
+        })
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            };
+        }
+    }
+}
+
+/// Error that gets returned when failing to create a thread pool
+#[derive(Debug, Clone)]
+pub struct PoolCreationError;
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "mininum threads must be greater than 0")
+    }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: Option<JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id: id,
+            thread: Some(thread),
+        }
+    }
+}
